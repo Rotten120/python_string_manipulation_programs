e697b8b3a35fed0225731613e6dc57e82ead39b6
Von <vonzedric012@gmail.com>
2025-03-19 14:25:16 +0800

    str_to_all_upper.py uploaded

---
+++ str_to_all_upper.py
@@ -0,0 +1,7 @@
+def str_to_all_upper_case():
+    str_inp = input("Input: ")
+    return str_inp.upper()
+
+if __name__ == "__main__":
+    str_out = str_to_all_upper_case()
+    print("Output:", str_out)
