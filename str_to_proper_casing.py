0b385e61ff3faabecb2623c5f681fb209b204c01
Von <vonzedric012@gmail.com>
2025-03-19 14:38:37 +0800

    fixed bug where program also adds a space before the first character

--- str_to_proper_casing.py
+++ str_to_proper_casing.py
@@ -1,9 +1,10 @@
 def str_to_proper_capital():
     str_inp = input("Input: ")
-    str_out = ""
-    
     str_arr = str_inp.split()
+    str_out = str_arr[0]
+    
     for word in str_arr:
+        if word == str_out: continue
         str_out += " " + word.capitalize()
     
     return str_out
