cdf6a4a93d6848d4eb317a163adec1dc7f943af5
Von <vonzedric012@gmail.com>
2025-03-19 14:25:50 +0800

    str_to_all_lower.py uploaded

---
+++ str_to_all_lower.py
@@ -0,0 +1,7 @@
+def str_to_all_lower_case():
+    str_inp = input("Input: ")
+    return str_inp.lower()
+
+if __name__ == "__main__":
+    str_out = str_to_all_lower_case()
+    print("Output:", str_out)
