a2397b12314780e80b99d21c796106e36f99b049
Von <vonzedric012@gmail.com>
2025-03-26 19:03:09 +0800

    fixed bug: first letter of the string is not capitalized

--- str_to_proper_casing.py
+++ str_to_proper_casing.py
@@ -1,11 +1,12 @@
 def str_to_proper_capital():
     str_inp = input("Input: ")
     str_arr = str_inp.split()
-    str_out = str_arr[0]
+    str_out = str_arr[0].capitalize()
     
     for word in str_arr:
-        if word == str_out: continue
-        str_out += " " + word.capitalize()
+        capitalized = word.capitalize()
+        if capitalized == str_out: continue
+        str_out += " " + capitalized
     
     return str_out
 
